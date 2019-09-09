4b05d5d15f6eeb059c1f853bc833604f06c7c75f
bruno <brmcerqueira@gmail.com>
2019-09-09 19:00:20 -0300

    Criação do Update

--- src/main/kotlin/com/brmcerqueira/kuerongo/Op.kt
+++ src/main/kotlin/com/brmcerqueira/kuerongo/Op.kt
@@ -5,6 +5,30 @@ object Op {
         "\$eq" *= JsonArray().put(left, right)
     }
 
+    fun cmp(left: Any?, right: Any?): IExpression = OperatorExpression {
+        "\$cmp" *= JsonArray().put(left, right)
+    }
+
+    fun gt(left: Any?, right: Any?): IExpression = OperatorExpression {
+        "\$gt" *= JsonArray().put(left, right)
+    }
+
+    fun gte(left: Any?, right: Any?): IExpression = OperatorExpression {
+        "\$gte" *= JsonArray().put(left, right)
+    }
+
+    fun lt(left: Any?, right: Any?): IExpression = OperatorExpression {
+        "\$lt" *= JsonArray().put(left, right)
+    }
+
+    fun lte(left: Any?, right: Any?): IExpression = OperatorExpression {
+        "\$lte" *= JsonArray().put(left, right)
+    }
+
+    fun ne(left: Any?, right: Any?): IExpression = OperatorExpression {
+        "\$ne" *= JsonArray().put(left, right)
+    }
+
     fun and(vararg expressions: IExpression): IExpression = OperatorExpression {
         "\$and" *= JsonArray().put(*expressions)
     }
